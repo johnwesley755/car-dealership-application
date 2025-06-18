24abf0d89135ae6111ba6ab114c0ba0db255b838
johnwesley755 <johnwesley8113@gmail.com>
2025-06-18 15:09:59 +0530

    seed file is updated

--- express-server/db.js
+++ express-server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/dealership';
+    const mongoURI = process.env.MONGO_URI || 'mongodb+srv://johnwesley7756:r84o9u1F4mPve9L6@cluster0.vzsd1tm.mongodb.net/';
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
